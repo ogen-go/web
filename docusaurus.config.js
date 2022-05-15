595d85b39aa3c93413011ae31619e92b1777f66a
Aleksandr Razumov <ernado@ya.ru>
2022-05-15 14:19:09 +0300

    feat: disable custom theming for code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'ogen',
@@ -112,10 +109,6 @@ const config = {
                         ],
                     }
                 ]
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
             },
         }),
 };
